a07840caa1465fd41e606abca0e6448b48261102
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-11-27 08:39:08 +1100

    exec kinda working

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,20 @@ impl BuildJob {
     fn prefix(&self) -> String {
         String::from("/home/kirillvr/.kitaman/pool/") + self.name().as_str()
     }
+
+    fn exec(&self, args: Vec<String>) {
+        let cmd = &args[0]; //TODO Panic
+        let rest_of_args: Vec<&String> = args.iter().skip(1).collect();
+        println!("{}", cmd);
+        println!("{:?}", rest_of_args);
+        process::Command::new(cmd)
+            .args(rest_of_args)
+            .status().expect("Failed exec");
+    }
 }
 
 fn main() {
+    //TODO proper args parsing
     let command = env::args().nth(1).expect("Please provide at least one argument");
 
     let job = BuildJob{source_directory: env::current_dir().expect("Failed to get current directory")};
@@ -62,5 +73,10 @@ fn main() {
     if command == "name" {
         let name = job.name();
         println!("{}", name);
+    }
+
+    if command == "exec" {
+        let args: Vec<String> = env::args().skip(2).collect();
+        job.exec(args);
     }
 }
